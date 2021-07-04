d72c8dfa959c49bbb3c240ee337a9482203195ef
Rafael Sousa <75494974+RafsRafado@users.noreply.github.com>
2021-07-04 16:43:23 +0000

    Update mysql.vb

---
+++ POS_Cashology/mysql.vb
@@ -0,0 +1,8 @@
+﻿Public Class mysql
+    Public Shared server As String = "localhost"
+     Public Shared user As String = "root"
+    Public Shared password As String = ""
+    Public Shared database As String = "poscashology"
+
+
+End Class
